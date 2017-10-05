4fd3222d61b084dc39652b0653b8423605ad1156
avanegasp <arco527@gmail.com>
2017-10-05 05:24:53 -0500

    All about strings II

--- strings.rb
+++ strings.rb
@@ -1,13 +1,20 @@
+#
+# rep = "a" * 5
+#
+# puts rep
+#
+# cadena1= "hola"
+# cadena2 = "mundo"
+#
+# puts cadena1 << cadena2
+#
+#
+# var1 = "hola mundo".gsub("hola", "angie")
+# puts var1
 
-rep = "a" * 5
 
-puts rep
+# var1 = "hola mundo"
+# puts var1.split(" ")
 
-cadena1= "hola"
-cadena2 = "mundo"
-
-puts cadena1 << cadena2
-
-
-var1 = "hola mundo".gsub("hola", "angie")
-puts var1
+var1 = ["hola", "mundo"]
+puts var1.join(" - ")
